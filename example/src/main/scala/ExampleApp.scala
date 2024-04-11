e24a6bd4dac8f172b43a139ed0f3921f63aa6810
amsen <ahph1380@gmail.com>
2024-04-11 12:51:58 -0400

    Add an example main for debugging

--- example/src/main/scala/ExampleApp.scala
+++ example/src/main/scala/ExampleApp.scala
@@ -1,37 +1,75 @@
-// /*
-//  * Copyright 2022 http4s.org
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *     http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
+package example
 
-// package example
+import org.http4s.curl.http.CurlRequest
+import org.http4s.curl.unsafe.CurlMultiRuntime
+import org.http4s.curl.http.simple.SimpleRequest
+import org.http4s.curl.http.simple.HttpVersion
+import org.http4s.curl.http.simple.HttpMethod
 
-// import cats.effect._
-// import cats.syntax.all._
-// import io.circe._
-// import org.http4s.circe.CirceEntityCodec._
-// import org.http4s.curl.CurlApp
+import scala.util.Success
+import gears.async.default.given
+import scala.concurrent.ExecutionContext
+import gears.async.*
+import scala.util.Failure
+import org.http4s.curl.unsafe.CurlRuntimeContext
+import scala.util.Try
+import org.http4s.curl.http.simple.SimpleResponse
+import scala.annotation.static
 
-// object ExampleApp extends CurlApp.Simple {
+object Example {
 
-//   final case class Joke(joke: String)
-//   object Joke {
-//     implicit val decoder: Decoder[Joke] = Decoder.forProduct1("joke")(Joke(_))
-//   }
+  given ExecutionContext = ExecutionContext.global
 
-//   def run: IO[Unit] = for {
-//     responses <- curlClient.expect[Joke]("https://icanhazdadjoke.com/").parReplicateA(3)
-//     _ <- responses.traverse_(r => IO.println(r.joke))
-//   } yield ()
+  def postRequest(msg: String)(using Async)(using CurlRuntimeContext) =
+    println("sending the post request")
+    CurlRequest(
+      SimpleRequest(
+        HttpVersion.V1_0,
+        HttpMethod.POST,
+        List(),
+        "http://localhost:8080/http/echo",
+        msg,
+      )
+    ) match
+      case Success(response) => assert(response.body == msg)
+      case Failure(exception) => throw exception
+    println("end sending the post request")
 
-// }
+  def getRequest()(using Async)(using CurlRuntimeContext) =
+    println("sending the get request")
+    val res = CurlRequest(
+      SimpleRequest(
+        HttpVersion.V1_0,
+        HttpMethod.GET,
+        List(),
+        "http://localhost:8080/http",
+        "",
+      )
+    ) match
+      case Success(response) =>
+        assert(response.body.nonEmpty)
+        response.body
+      case Failure(exception) =>
+        throw exception
+    println("end sending the get request")
+    res
+
+  def request()(using Async)(using CurlRuntimeContext) =
+    postRequest(getRequest())
+
+  def getTime[T](body: () => T) =
+    val time = System.currentTimeMillis()
+    body()
+    (System.currentTimeMillis() - time)
+
+
+  def test(): Unit = {
+    CurlMultiRuntime:
+      Async.blocking:
+        val elems = (0 to 0)
+        // Sync
+        // println("Sync time: " + getTime(() => elems.map(_ => request())))
+        // Async
+        println("Async time: " + getTime(() => elems.map(_ => Future(request())).awaitAll))
+  }
+}
