d6c600c7e88919f6d9f58f57c13fc2ada8a196d8
amsen <ahph1380@gmail.com>
2024-08-31 09:34:48 -0400

    Fix runtime test

--- purl/src/test/scala/http/CurlRequestSuite.scala
+++ purl/src/test/scala/http/CurlRequestSuite.scala
@@ -1,8 +1,7 @@
 package purl
 
-import gears.async.default.given
-import gears.async.Async
 import pollerBear.runtime._
+import purl.global.CurlGlobal
 import purl.http.simple.{ HttpMethod, HttpVersion, SimpleRequest }
 import purl.http.CurlRequest
 import purl.multi.CurlMultiRuntime
@@ -17,104 +16,112 @@ class CurlRequestSuite extends munit.FunSuite {
 
   test("simple get request") {
     var done = false
-    withPassivePoller { poller =>
-      given PassivePoller = poller
-      CurlMultiRuntime:
-        CurlRequest(
-          SimpleRequest(
-            HttpVersion.V1_0,
-            HttpMethod.GET,
-            List(),
-            "http://localhost:8080/http",
-            "".getBytes()
-          )
-        ) {
-          case Success(response) =>
-            assert(response.body.nonEmpty)
-            done = true
-          case Failure(exception) =>
-            fail("couldn't get the response", exception)
-        }
-
-        while !done do poller.waitUntil()
-    }
-  }
-
-  test("status code") {
-    var done = false
-    withPassivePoller { poller =>
-      given PassivePoller = poller
-      CurlMultiRuntime:
-        for statusCode <- List(404, 500) do
-          done = false
+    CurlGlobal:
+      withPassivePoller { poller =>
+        given PassivePoller = poller
+        CurlMultiRuntime:
           CurlRequest(
             SimpleRequest(
-              HttpVersion.V1_0,
+              HttpVersion.V1_1,
               HttpMethod.GET,
               List(),
-              "http://localhost:8080/http/" + statusCode.toString,
+              "http://localhost:8080/http",
               "".getBytes()
             )
           ) {
             case Success(response) =>
-              assertEquals(response.status, statusCode)
+              assert(response.body.nonEmpty)
               done = true
             case Failure(exception) =>
               fail("couldn't get the response", exception)
+              done = true
           }
 
           while !done do poller.waitUntil()
-    }
+      }
+  }
+
+  test("status code") {
+    var done = false
+    CurlGlobal:
+      withPassivePoller { poller =>
+        given PassivePoller = poller
+        CurlMultiRuntime:
+          for statusCode <- List(404, 500) do
+            done = false
+            CurlRequest(
+              SimpleRequest(
+                HttpVersion.V1_1,
+                HttpMethod.GET,
+                List(),
+                "http://localhost:8080/http/" + statusCode.toString,
+                "".getBytes()
+              )
+            ) {
+              case Success(response) =>
+                assertEquals(response.status, statusCode)
+                done = true
+              case Failure(exception) =>
+                fail("couldn't get the response", exception)
+                done = true
+            }
+
+            while !done do poller.waitUntil()
+      }
   }
 
   test("error") {
     var done = false
-    withPassivePoller { poller =>
-      given PassivePoller = poller
-      CurlMultiRuntime:
-        CurlRequest(
-          SimpleRequest(
-            HttpVersion.V1_0,
-            HttpMethod.GET,
-            List(),
-            "unsupported://server",
-            "".getBytes()
-          )
-        ) {
-          case Success(response) =>
-            fail("should have failed")
-          case Failure(exception) =>
-            assert(exception.isInstanceOf[CurlError])
-            done = true
-        }
+    CurlGlobal:
+      withPassivePoller { poller =>
+        given PassivePoller = poller
+        CurlMultiRuntime:
+          CurlRequest(
+            SimpleRequest(
+              HttpVersion.V1_1,
+              HttpMethod.GET,
+              List(),
+              "unsupported://server",
+              "".getBytes()
+            )
+          ) {
+            case Success(response) =>
+              fail("should have failed")
+              done = true
+            case Failure(exception) =>
+              assert(exception.isInstanceOf[CurlError])
+              done = true
+          }
 
-        while !done do poller.waitUntil()
-    }
+          while !done do poller.waitUntil()
+      }
   }
 
   test("post echo") {
     var done = false
-    withPassivePoller { poller =>
-      given PassivePoller = poller
-      CurlMultiRuntime:
-        for msg <- List("SOME_NOT_RANDOM_STRING_JRWIGJER)$%)##@R") do
-          CurlRequest(
-            SimpleRequest(
-              HttpVersion.V1_0,
-              HttpMethod.POST,
-              List(),
-              "http://localhost:8080/http/echo",
-              msg.getBytes()
-            )
-          ) {
-            case Success(response) =>
-              assert(response.body.map(_.toChar).mkString.contains(msg))
-              done = true
-            case Failure(exception) =>
-              fail("couldn't get the response", exception)
-          }
+    CurlGlobal:
+      withPassivePoller { poller =>
+        given PassivePoller = poller
+        CurlMultiRuntime:
+          for msg <- List("SOME_NOT_RANDOM_STRING_JRWIGJER)$%)##@R") do
+            CurlRequest(
+              SimpleRequest(
+                HttpVersion.V1_1,
+                HttpMethod.POST,
+                List(),
+                "http://localhost:8080/http/echo",
+                msg.getBytes()
+              )
+            ) {
+              case Success(response) =>
+                assert(response.body.map(_.toChar).mkString.contains(msg))
+                done = true
+              case Failure(exception) =>
+                fail("couldn't get the response", exception)
+                done = true
+            }
 
-          while !done do poller.waitUntil()
-    }
+            while !done do poller.waitUntil()
+      }
   }
 }
